367ff2c6d74af59742b604c0c92bfe85dc949e95
minnnisu <mine52100@naver.com>
2024-06-20 10:11:04 +0900

    first commit

---
+++ src/main/java/org/minnnisu/deploymentstudy/DeploymentStudyApplication.java
@@ -0,0 +1,13 @@
+package org.minnnisu.deploymentstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploymentStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeploymentStudyApplication.class, args);
+    }
+
+}
