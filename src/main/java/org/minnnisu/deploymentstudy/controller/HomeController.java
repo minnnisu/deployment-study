7ea6ea890222e1380f3eb8a782fe5fcc0871436b
minnnisu <mine52100@naver.com>
2024-06-20 21:01:33 +0900

    여러가지 API 추가

--- src/main/java/org/minnnisu/deploymentstudy/controller/HomeController.java
+++ src/main/java/org/minnnisu/deploymentstudy/controller/HomeController.java
@@ -2,6 +2,8 @@ package org.minnnisu.deploymentstudy.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,6 +14,16 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "Hello CICD! version: " + version;
+        return "home!";
+    }
+
+    @GetMapping("/version")
+    public String version() {
+        return "version: " + version;
+    }
+
+    @GetMapping("/friendship")
+    public String test() {
+        return "우정 영원하자!";
     }
 }
